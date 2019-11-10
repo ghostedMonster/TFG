1c84df998aaa00a65d3cb798fda8c34bc837bf04
ghostedMonster <jose-antonio-1110@hotmail.com>
2019-11-10 11:14:09 +0100

    Añadidas las imagenes de cartas y pruebas de interfaces de usuario en PyQt

---
+++ cartas/main.py
@@ -0,0 +1,48 @@
+from cartas.juego import Juego
+
+
+def main():
+    g = Juego()
+    print('Baraja: {}'.format(g.baraja))
+    print('Jugadas: {}'.format(g.jugadas))
+    print('Ganadas por el Jugador 1: {}'.format(g.player_1.won))
+    print('Lote del Jugador 1: {}'.format(g.player_1.lot))
+    print('Ganadas por el Jugador 2: {}'.format(g.player_2.won))
+    print('Lote del Jugador 2: {}'.format(g.player_2.lot))
+    print('Ganadas por el Jugador 3: {}'.format(g.player_3.won))
+    print('Lote del Jugador 3: {}'.format(g.player_3.lot))
+    print('Ganadas por el Jugador 4: {}'.format(g.player_4.won))
+    print('Lote del Jugador 4: {}'.format(g.player_4.lot))
+    print('Ganadas por el Jugador 5: {}'.format(g.player_5.won))
+    print('Lote del Jugador 5: {}'.format(g.player_5.lot))
+    g.barajar()
+    print('Tras barajar las cartas, la baraja ha quedado así: \n')
+    print(g.baraja)
+    print('Repartimos las cartas: ')
+    g.repartir()
+
+    print('Lote del Jugador 1: {}'.format(g.player_1.lot))
+    print('Lote del Jugador 2: {}'.format(g.player_2.lot))
+    print('Lote del Jugador 3: {}'.format(g.player_3.lot))
+    print('Lote del Jugador 4: {}'.format(g.player_4.lot))
+    print('Lote del Jugador 5: {}'.format(g.player_5.lot))
+
+    print('Cartas que sobran: {}'.format(g.baraja))
+
+    print('Ordenamos los lotes de los jugadores: ')
+
+    g.ordenar(g.player_1.lot)
+    g.ordenar(g.player_2.lot)
+    g.ordenar(g.player_3.lot)
+    g.ordenar(g.player_4.lot)
+    g.ordenar(g.player_5.lot)
+
+    print('Lote del Jugador 1: {}'.format(g.player_1.lot))
+    print('Lote del Jugador 2: {}'.format(g.player_2.lot))
+    print('Lote del Jugador 3: {}'.format(g.player_3.lot))
+    print('Lote del Jugador 4: {}'.format(g.player_4.lot))
+    print('Lote del Jugador 5: {}'.format(g.player_5.lot))
+
+
+if __name__ == "__main__":
+    main()
